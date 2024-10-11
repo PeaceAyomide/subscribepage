9f181d529c12d89a091087cad1a7ae073c3fff46
Peace Ayomide <helaralex2000@gmail.com>
2024-10-11 10:30:50 +0100

    new

--- src/Firstpage.jsx
+++ src/Firstpage.jsx
@@ -26,6 +26,14 @@ const Firstpage = () => {
         <p className='text-[1.3rem] mt-2 IPad:text-[1rem] side-phone:text-[0.8rem] phone:text-[0.6rem] font-calibril'>
         You are cordially invited to the exclusive launch of GETMEDCO's state-of-the-art Concierge Clinic, where VIP-level healthcare meets cutting-edge innovation.
         </p>
+        <div className='flex justify-center mt-6'>
+          <a href="https://sfxkxgch21h.typeform.com/to/xgtoZ6CS" target="_blank" rel="noopener noreferrer">
+            <button className='bg-[#F6B25F] font-bold text-black py-2 px-[3rem] IPad:px-[2rem] side-phone:px-[1rem] phone:px-[0.5rem] text-[1.5rem] IPad:text-[1rem] side-phone:text-[0.8rem] phone:text-[0.6rem] rounded-lg font-calibril hover:bg-[#e69a4d]'>
+              RSVP
+            </button>
+          </a>
+        </div>
+        
         </div>
 
         <div className='two flex-1 p-[3rem] side-phone:p-[1rem] self-start side-phone:text-center side-phone:w-full'>
@@ -34,7 +42,7 @@ const Firstpage = () => {
         <span className='font-blacksword text-[#F6B25F]'>Time:</span> 11 am-2 pm
         </p>
         <p className='text-[1.3rem] mt-2 IPad:text-[1rem] side-phone:text-[0.8rem] phone:text-[0.6rem] font-calibril'>
-        <span className='font-blacksword text-[#F6B25F]'>Address:</span> Block 8, Plot 6A Wumego Crescent off Admiralty Road, off Christ Avenue, Lekki Phase 1, Lagos <br /> Gooogle map :
+        <span className='font-blacksword text-[#F6B25F]'>Address:</span> Block 8, Plot 6A Wumego Crescent off Admiralty Road, off Christ Avenue, Lekki Phase 1, Lagos
         <div className='flex justify-center items-center mt-[2rem] IPad:mt-[1rem] side-phone:mt-[1rem] phone:mt-[0.5rem]'>
           <iframe
             className='w-full max-w-[40rem] h-[20rem] border-0 rounded-lg shadow-lg IPad:max-w-[20rem] side-phone:max-w-[15rem] phone:max-w-[10rem] IPad:h-[15rem] side-phone:h-[10rem] phone:h-[5rem] '
